a0eb3e06bfa9512064173df6ffe3a55d97dca785
Your Name <you@example.com>
2021-01-04 16:48:00 -0200

    04-01-2021

--- app/models/servicos_interno.rb
+++ app/models/servicos_interno.rb
@@ -5,8 +5,10 @@ class ServicosInterno < ActiveRecord::Base
 def geracodigo
     #self.codigo = [self.id-782].to_s + ("/2017")
     #self.codigo = [self.id-1124].to_s + ("/2018")   # igual ao ultimo registro de 2017
-    self.codigo = [self.id-1483].to_s + ("/2019")   # igual ao ultimo registro de 2018
-    self.codigo = [self.id-1757].to_s + ("/2020")   # igual ao ultimo registro de 2019
+    #self.codigo = [self.id-1483].to_s + ("/2019")   # igual ao ultimo registro de 2018
+    #self.codigo = [self.id-1757].to_s + ("/2020")   # igual ao ultimo registro de 2019
+    #self.codigo = [self.id-1757].to_s + ("/2020")   # igual ao ultimo registro de 2020
+    self.codigo = [self.id-1841].to_s + ("/2021")   # igual ao ultimo registro de 2019
     self.save
 
 end
