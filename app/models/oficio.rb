97870af18b2b29497644af5c2a6766311d2679b0
Your Name <you@example.com>
2021-01-04 09:23:32 -0200

    04-01-2021

--- app/models/oficio.rb
+++ app/models/oficio.rb
@@ -6,7 +6,9 @@ def geracodigo
     #self.codigo = [self.id-612].to_s + ("/2017")
     #self.codigo = [self.id-969].to_s + ("/2018")
     #self.codigo = [self.id-1360].to_s + ("/2019")
-    self.codigo = [self.id-1802].to_s + ("/2020")
+    #self.codigo = [self.id-1802].to_s + ("/2020")
+    self.codigo = [self.id-1904].to_s + ("/2021")
+
     self.save
 
 end
